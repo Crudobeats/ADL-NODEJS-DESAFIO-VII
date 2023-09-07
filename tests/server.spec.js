e7b2e1ac5fdb52daa67097f33d489576942235c6
Victor Rodriguez <divictorrodriguez@gmail.com>
2023-09-06 21:23:29 -0300

    Fix error common

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,5 +1,5 @@
-import request from "supertest";
-import server from "../index";
+const request = require("supertest");
+const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
   //     Testea que la ruta GET /cafes devuelve un status
